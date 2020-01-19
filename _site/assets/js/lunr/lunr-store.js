9d6070044675e6631eaa500e36d3a3c98c7aa541
MarinaSergeeva <sergeeva.marin@gmail.com>
2020-01-19 18:41:30 -0500

    fix main page

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"Hello World!!  ","categories": ["test"],
+        "tags": [],
+        "url": "http://localhost:4000/test/welcome-to-jekyll/",
+        "teaser":null}]
