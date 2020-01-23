511f51f0b62f283a4841847a027bc187861ef19a
MarinaSergeeva <sergeeva.marin@gmail.com>
2020-01-23 00:02:04 -0500

    removed placeholder post

--- _posts/2020-01-14-welcome-to-jekyll.markdown
+++
@@ -1,8 +0,0 @@
----
-layout: single
-title:  "Hello World!"
-date:   2020-01-14 21:54:40 -0500
-categories: test
----
-
-Hello World!!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,9 +1,4 @@
 var store = [{
-        "title": "Hello World!",
-        "excerpt":"Hello World!!  ","categories": ["test"],
-        "tags": [],
-        "url": "http://localhost:4000/test/welcome-to-jekyll/",
-        "teaser":null},{
         "title": "A step by step guide on solving problems",
         "excerpt":"Being a data scientist and an educator, I’m solving problems all day every day and regularly help others to do that. In this guide I’ll outline a structure to help you solve problems as well. Although it’s mostly focused on the professional situations, such as interviews or building projects while...","categories": ["problem_solving","algorithms","interviews"],
         "tags": [],
